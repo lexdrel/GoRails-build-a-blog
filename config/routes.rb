394b160a91e99bc5715f605da6f68fd32ecb9a1f
Alex Castillo <alex@lexdrel.com>
2026-03-03 20:32:36 +0000

    update new

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "blog_posts#index"
 
-  get "/blog_posts/:id", to: "blog_posts#show", as: "blog_post"
   get "/blog_posts/new", to: "blog_posts#new", as: "new_blog_post"
+  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: "edit_blog_post"
+  get "/blog_posts/:id", to: "blog_posts#show", as: "blog_post"
+  post "/blog_posts", to: "blog_posts#create", as: "blog_posts"
+  patch "/blog_posts/:id", to: "blog_posts#update"
 end

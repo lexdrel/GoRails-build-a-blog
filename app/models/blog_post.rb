3714594e474fa3e059d696bc3894256f5fa56e8b
Alex Castillo <alex@lexdrel.com>
2026-03-05 19:50:27 +0000

    blogpost model tests with fixtures

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,10 +2,22 @@ class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
-  scope :sorted, -> { order(published_at: :desc) }
+  scope :sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(published_at: :desc) }
   scope :draft, -> { where(published_at: nil) }
   scope :published, -> { where("published_at <= ?", Time.current) }
   scope :scheduled, -> { where("published_at > ?", Time.current) }
+
+  def draft?
+    published_at.nil?
+  end
+
+  def published?
+    published_at? && published_at <= Time.current
+  end
+
+  def scheduled?
+    published_at? && published_at > Time.current
+  end
 end
 
 # BlogPost.all is a default scope
@@ -13,3 +25,5 @@ end
 # BlogPost.published is a another scope
 # BlogPost.scheduled is a another scope
 # BlogPost.published.sorted
+# blog_post = BlogPost.arel_table
+# lexdrelBlogPosts = BlogPost.order(blog_post[:published_at].desc.nulls_last)
--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -4,4 +4,22 @@ class BlogPostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "draft? returns true to draft posts" do
+    # assert BlogPost.new(published_at: nil).draft?
+    assert blog_posts(:draft).draft? # it takes the fixtures draft: in the yaml file
+  end
+
+  test "draft? returns false to non-draft posts" do
+    # refute BlogPost.new(published_at: 1.year.ago).draft?
+    refute blog_posts(:published).draft?
+
+    # arel_table basic example
+    user = User.arel_table
+    lexdrelUsers = User.where(user[:email].matches("%lexdrel%"))
+    # this will return all users with email that contains lexdrel
+
+    blog_post = BlogPost.arel_table
+    lexdrelBlogPosts = BlogPost.order(blog_post[:published_at].desc.nulls_last)
+  end
 end
